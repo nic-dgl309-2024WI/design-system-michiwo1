aab951dd01ad9be9fb266dba29875e26c2afaf8f
michiwo <mk1104kado@gmail.com>
2024-02-02 23:30:00 -0800

    Add navigation to mobile display

--- design-system.js
+++ design-system.js
@@ -1,6 +1,7 @@
 var hamburgerMenuIcon = document.getElementById('hamburger-menu-icon');
 var closeIcon = document.getElementById('close-icon');
 var sidebar = document.getElementById('sidebar');
+var nav = document.getElementById('nav');
 
 // When the page loads, show the hamburger menu icon and hide the close icon
 window.onload = function() {
@@ -11,11 +12,13 @@ window.onload = function() {
 hamburgerMenuIcon.addEventListener('click', function() {
   sidebar.classList.toggle('is-active');
   hamburgerMenuIcon.style.display = 'none';
-  closeIcon.style.display = 'block';
+  closeIcon.style.display = 'inline-block';
+  nav.style.display = 'block';
 });
 
 closeIcon.addEventListener('click', function () {
   sidebar.classList.toggle('is-active');
   hamburgerMenuIcon.style.display = 'block';
   closeIcon.style.display = 'none';
+  nav.style.display = 'none';
 });
