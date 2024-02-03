ca54ac124b7473ff4cf9035efd4b115d9e966371
michiwo <mk1104kado@gmail.com>
2024-02-02 23:50:05 -0800

    Refactor design system CSS and JavaScript

--- design-system.js
+++ design-system.js
@@ -3,12 +3,6 @@ var closeIcon = document.getElementById('close-icon');
 var sidebar = document.getElementById('sidebar');
 var nav = document.getElementById('nav');
 
-// When the page loads, show the hamburger menu icon and hide the close icon
-window.onload = function() {
-  hamburgerMenuIcon.style.display = 'block';
-  closeIcon.style.display = 'none';
-};
-
 hamburgerMenuIcon.addEventListener('click', function() {
   sidebar.classList.toggle('is-active');
   hamburgerMenuIcon.style.display = 'none';
