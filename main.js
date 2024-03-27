634781d4251a4ce927945bd77fc8f54da797f17e
michiwo <mk1104kado@gmail.com>
2024-03-27 16:03:24 -0700

    Add hero section

--- main.js
+++ main.js
@@ -2,3 +2,25 @@
 document.querySelector('.toggle').addEventListener('click', function () {
   document.querySelector('nav').classList.toggle('show');
 });
+
+// carousel
+var carousel = document.querySelector('.carousel-images');
+var images = Array.from(carousel.children);
+var prevButton = document.querySelector('.carousel-control.prev');
+var nextButton = document.querySelector('.carousel-control.next');
+var currentIndex = 0;
+
+function updateCarousel() {
+  var newTransformValue = -currentIndex * 100 / images.length;
+  carousel.style.transform = 'translateX(' + newTransformValue + '%)';
+}
+
+prevButton.addEventListener('click', function() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', function() {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateCarousel();
+});
